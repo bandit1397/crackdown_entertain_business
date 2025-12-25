7be7c95c7a7b7a36a27a092a989265b6c6434f1f
bandit <80446239+bandit1397@users.noreply.github.com>
2025-12-25 16:26:34 +0900

    Update data.js

--- data.js
+++ data.js
@@ -217,8 +217,218 @@ const data = {
     { text:"", depth:2 }
        ]
 }
+],
 
+"노래연습장":[
+{
+  name: { text:"무등록 영업", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제18조 제1항", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제3항 제1호", depth:2 }
+  ],
+
+  penalty:[
+    { text:"2년 이하 징역 또는 2천만 이하 벌금", depth:2 },
+    { text:"폐쇄", depth:2 }
+  ],
+
+  note:[
+    { text:"", depth:2 },
+    { text:"", depth:2 }
+  ]
+},
+
+{
+  name: { text:"영업정지 중 영업", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제27조 제1항 제2호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제3항 제3호", depth:2 }
+  ],
+
+  penalty:[
+    { text:"2년 이하 징역 또는 2천만원 이하 벌금", depth:2 },
+    { text:"취소 폐쇄", depth:2 }
+  ],
+
+  note:[
+    { text:"", depth:2 },
+    { text:"", depth:2 }
+  ]
+},
+
+{
+  name: { text:"주류판매", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제1항 제3호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제3항 제2호", depth:2 }
+  ],
+
+  penalty:[
+    { text:"2년 이하 징역 또는 2천만 이하 벌금", depth:2 },
+    { text:"10일/1월/3월/취소 폐쇄", depth:2 }
+  ],
+
+  note:[
+    { text:"", depth:2 },
+    { text:"", depth:2 }
+  ]
+},
+
+{
+  name: { text:"청소년 시간외 출입", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제1항 제2호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제3항 제2호", depth:2 }
+      ],
   
+  penalty:[
+    { text:"2년 이하 징역 또는 2천만 이하 벌금", depth:2 },
+    { text:"10일/1월/3월/취소 폐쇄", depth:2 }
+         ],
+
+  note:[
+    { text:"이때 청소년은 18세 미만", depth:2 },
+    { text:"", depth:2 }
+       ]
+},
+
+
+{
+  name: { text:"청소년실 외 청소년출입", depth:0 },
+
+  law:[
+    { text:"청소년보호법 제29조 제2항", depth:2 },
+    { text:"청소년보호법 제59조 제8호", depth:2 }
+      ],
+
+  penalty:[
+    { text:"2년 이하 징역 또는 2천만원 이하 벌금", depth:2 },
+    { text:"10일/20일/1월/3월", depth:2 }
+          ],
+
+  note:[
+    { text:"청소년은 연19세 미만", depth:2 },
+    { text:"", depth:2 }
+      ]
+},
+
+{
+  name: { text:"청소년 접대부 고용", depth:0 },
+
+  law:[
+    { text:"청소년보호법 제30조 2항", depth:2 },
+    { text:"청소년보호법 제56조", depth:2 }
+      ],
+
+  penalty:[
+    { text:"10년 이하 징역", depth:2 },
+    { text:"취소", depth:2 }
+          ],
+
+  note:[
+    { text:"", depth:2 },
+    { text:"", depth:2 }
+       ]
+},
+
+{
+  name: { text:"성인 접대부 고용", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제1항 제4호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제2항", depth:2 }
+      ],
+
+  penalty:[
+    { text:"3년 이하 징역 또는 3천만 이하 벌금", depth:2 },
+    { text:"1월/2월/취소 폐쇄", depth:2 }
+          ],
+
+  note:[
+    { text:"접대부 남녀 불문", depth:2 },
+    { text:"", depth:2 }
+       ]
+},
+
+{
+  name: { text:"취업한 종업원", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제2항", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제4항", depth:2 }
+      ],
+
+  penalty:[
+    { text:"1년 이하 징역 또는 3백만 이하 벌금", depth:2 },
+    { text:"", depth:2 }
+          ],
+
+  note:[
+    { text:"접대부 남녀 불문", depth:2 },
+    { text:"", depth:2 }
+       ]
+},
+
+{
+  name: { text:"호객행위", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제1항 제4호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 제34조 제2항", depth:2 }
+      ],
+
+  penalty:[
+    { text:"3년 이하 징역 또는 3천만 이하 벌금", depth:2 },
+    { text:"10일/20일/1월/3월", depth:2 }
+          ],
+
+  note:[
+    { text:"", depth:2 },
+    { text:"", depth:2 }
+       ]
+},
+
+{
+  name: { text:"주류 보관(반입묵인)", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제1항 제6호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 시행령 제9조(별표 1)", depth:2 }
+      ],
+
+  penalty:[
+    { text:"벌칙 X, 과징금 O", depth:2 },
+    { text:"10일/20일/1월/3월", depth:2 }
+          ],
+
+  note:[
+    { text:"접대부 남녀 불문", depth:2 },
+    { text:"", depth:2 }
+       ]
+},
+
+{
+  name: { text:"등록증 미부착", depth:0 },
+
+  law:[
+    { text:"음악산업진흥에 관한 법률 제22조 제1항 제6호", depth:2 },
+    { text:"음악산업진흥에 관한 법률 시행령 제9조(별표 1)", depth:2 }
+      ],
+
+  penalty:[
+    { text:"벌칙 X, 과징금 X", depth:2 },
+    { text:"경고/10일/20일1월", depth:2 }
+          ],
+
+  note:[
+    { text:"", depth:2 },
+    { text:"", depth:2 }
+       ]
+}
 ]
 
 };
